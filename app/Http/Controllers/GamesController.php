a220b6980bfab88436e4239f6530108828875377
JuanAndProg <juanandresmunoz85@gmail.com>
2023-07-27 18:04:34 +0200

    GameForm - definition&tests form requests

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\StoreGame;
+use App\Models\Game;
+
 class GamesController extends Controller
 {
     public function __invoke(){
         return view('games');
     }
+    public function create(){
+        return view('games');
+    }
+    public function store(StoreGame $request){
+        
+        $game = Game::create($request->all());
+    }
+
 }
